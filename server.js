1c8228db8fc671c1b61dd3d7e65ec94af70ca44e
vladguzun <vladguzun20022@gmail.com>
2024-02-27 15:29:55 -0800

    commmageagseg

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://client-seven-gilt.vercel.app",
     methods: ["GET", "POST"]
   }
 });
