26a40137d6dad024d0fe07e5d68d53d8d4434c28
vladguzun <vladguzun20022@gmail.com>
2024-02-27 14:04:55 -0800

    c

---
+++ server.ts
@@ -0,0 +1,41 @@
+const { Server } = require('socket.io');
+const http = require('http');
+
+const server = http.createServer();
+const io = new Server(server, {
+    cors: "http://localhost:3000",
+    methods: ["POST", "GET"]
+});
+
+const connectedClients = new Set();
+
+
+
+
+io.on('connection', (socket:any) => {
+    console.log('A user connected:', socket.id);
+    connectedClients.add(socket);
+    const clientsArray:any[] = [...connectedClients];
+    console.log('_____');
+    clientsArray.map(client => console.log(client.id));
+
+    
+
+
+
+    socket.on('notification', (senderSocketId:any) => {
+        console.log('Received notification from:', senderSocketId);
+
+        // Send a notification to all connected clients except the sender
+        socket.broadcast.emit("notification", senderSocketId);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+        connectedClients.delete(socket);
+    });
+});
+
+server.listen(3001, () => {
+    console.log('Server listening on port 3001');
+});
